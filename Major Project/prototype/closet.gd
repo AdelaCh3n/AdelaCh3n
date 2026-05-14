d921757e31340a3770db5cefd7677a1a4908c443
Adela <7011250@spsd.sk.ca>
2026-05-14 16:32:16 -0600

    5/14 update -outfit code
    outfit code feature added. now you can change the outfit by entering a code

--- Major Project/prototype/closet.gd
+++ Major Project/prototype/closet.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 
 @onready var clothes = $clothes
-
+@onready var code_input = $"../CodeInput"
+@onready var code_label = $"../CodeLabel"
+	
 #wearing variable
 @onready var hair = $"../Person/hair"
 @onready var tops = $"../Person/tops"
@@ -19,10 +21,13 @@ var current_dresses = -1
 var current_socks = -1
 var current_shoes = -1
 
+
+
 func _ready():
 	hide_closet()
 	hide_wearing()
 	
+
 	
 func hide_closet(): #hide all the clothes(in closet) before started
 	for c in clothes.get_children():
@@ -43,13 +48,48 @@ func hide_wearing():#hide all the clothes(wearing) before started
 		sh.visible = false
 		
 
+func generate_code():
+	return (
+		index_to_code(current_hair)
+		+index_to_code(current_tops)
+		+index_to_code(current_bottoms)
+		+index_to_code(current_dresses)
+		+index_to_code(current_socks)
+		+index_to_code(current_shoes)
+	)
+
+func update_code():
+	code_label.text = generate_code()
+
+func load_code(code):
+	
+	wear_hair(e_code(code[0]),false)
+	wear_dresses(e_code(code[3]),false)
+	wear_top(e_code(code[1]),false)
+	wear_bottoms(e_code(code[2]),false)
+	
+	wear_socks(e_code(code[4]),false)
+	wear_shoes(e_code(code[5]),false)
+	
+func e_code(index): #enter e means "not wearing"
+	if index == "e":
+		return -1
+	return int(index)
+	
+func index_to_code(index):
+	if index == -1:
+		return "e"
+		
+	return str(index)
+
+
 func show_clothes(index):#show clothes from the index
 	for i in range(clothes.get_child_count()):
 		var c = clothes.get_child(i)
 		c.visible = (i == index)
 		
-func wear_hair(index):
-	if current_hair == index:#click again to unwear
+func wear_hair(index, toggle = true):
+	if toggle==true and current_hair == index:#click again to unwear
 		current_hair = -1
 		for h in hair.get_children():
 			h.visible = false
@@ -58,10 +98,12 @@ func wear_hair(index):
 	for i in range(hair.get_child_count()):
 		var h = hair.get_child(i)
 		h.visible = (i == index)
+	update_code()
 
-func wear_top(index):
+func wear_top(index, toggle = true):
 	hide_group(dresses)
-	if current_tops == index:#click again to unwear
+	current_dresses = -1
+	if toggle==true and current_tops == index:#click again to unwear
 		current_tops = -1
 		for t in tops.get_children():
 			t.visible = false
@@ -70,10 +112,12 @@ func wear_top(index):
 	for i in range(tops.get_child_count()):#click to wear
 		var t = tops.get_child(i)
 		t.visible = (i == index)
+	update_code()
 		
-func wear_bottoms(index):
+func wear_bottoms(index, toggle = true):
 	hide_group(dresses)
-	if current_bottoms == index:#click again to unwear
+	current_dresses = -1
+	if toggle==true and current_bottoms == index:#click again to unwear
 		current_bottoms = -1
 		for b in bottoms.get_children():
 			b.visible = false
@@ -82,11 +126,14 @@ func wear_bottoms(index):
 	for i in range(bottoms.get_child_count()):
 		var b = bottoms.get_child(i)
 		b.visible = (i == index)
+	update_code()
 		
-func wear_dresses(index):
+func wear_dresses(index, toggle = true):
 	hide_group(tops)
 	hide_group(bottoms)
-	if current_dresses == index:#click again to unwear
+	current_bottoms = -1
+	current_tops = -1
+	if toggle==true and current_dresses == index:#click again to unwear
 		current_dresses = -1
 		for d in dresses.get_children():
 			d.visible = false
@@ -95,9 +142,10 @@ func wear_dresses(index):
 	for i in range(dresses.get_child_count()):
 		var d = dresses.get_child(i)
 		d.visible = (i == index)
+	update_code()
 		
-func wear_socks(index):
-	if current_socks == index:#click again to unwear
+func wear_socks(index, toggle = true):
+	if toggle==true and current_socks == index:#click again to unwear
 		current_socks = -1
 		for so in socks.get_children():
 			so.visible = false
@@ -106,9 +154,10 @@ func wear_socks(index):
 	for i in range(socks.get_child_count()):
 		var so = socks.get_child(i)
 		so.visible = (i == index)
+	update_code()
 		
-func wear_shoes(index):
-	if current_shoes == index:#click again to unwear
+func wear_shoes(index, toggle = true):
+	if toggle==true and current_shoes == index:#click again to unwear
 		current_shoes = -1
 		for sh in tops.get_children():
 			sh.visible = false
@@ -117,6 +166,7 @@ func wear_shoes(index):
 	for i in range(shoes.get_child_count()):
 		var sh = shoes.get_child(i)
 		sh.visible = (i == index)
+	update_code()
 
 func hide_group(group):
 	for i in group.get_children():
@@ -143,42 +193,65 @@ func _on_5_pressed() -> void:
 
 func _on_0a_pressed() -> void:
 	wear_hair(0);
-
+	update_code()
 
 func _on_0b_pressed() -> void:
 	wear_hair(1)
-
+	update_code()
 
 func _on_1a_pressed() -> void:
 	wear_top(0);
-
+	update_code()
 
 func _on_1b_pressed() -> void:
 	wear_top(1);
-
+	update_code()
 
 func _on_2a_pressed() -> void:
 	wear_bottoms(0);
-
+	update_code()
 
 func _on_2b_pressed() -> void:
 	wear_bottoms(1);
-
+	update_code()
 
 func _on_3a_pressed() -> void:
 	wear_dresses(0);
+	update_code()
 
 func _on_3b_pressed() -> void:
 	wear_dresses(1);
+	update_code()
 
 func _on_4a_pressed() -> void:
 	wear_socks(0);
+	update_code()
 
 func _on_4b_pressed() -> void:
 	wear_socks(1);
+	update_code()
 	
 func _on_5a_pressed() -> void:
 	wear_shoes(0);
+	update_code()
 
 func _on_5b_pressed() -> void:
 	wear_shoes(1);
+	update_code()
+
+
+func _on_reset_pressed() -> void:
+	wear_hair(-1);
+	wear_top(-1);
+	wear_bottoms(-1);
+	wear_dresses(-1);
+	wear_socks(-1);
+	wear_shoes(-1);	
+
+func _on_random_pressed() -> void:
+	pass # Replace with function body.
+	
+
+func _on_load_button_pressed() -> void:
+	var code = code_input.text 
+	load_code(code)
