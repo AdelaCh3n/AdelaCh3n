e17ae0af63fb3c6ec6bb3f19134b463e2fcb81bf
Adela <7011250@spsd.sk.ca>
2026-05-13 22:23:08 -0600

    5/13 Update
    Now you can remove clothes by reclick the button

--- Major Project/prototype/closet.gd
+++ Major Project/prototype/closet.gd
@@ -3,12 +3,21 @@ extends Node2D
 
 @onready var clothes = $clothes
 
+#wearing variable
 @onready var hair = $"../Person/hair"
 @onready var tops = $"../Person/tops"
 @onready var bottoms = $"../Person/bottoms"
 @onready var dresses = $"../Person/dresses"
 @onready var socks = $"../Person/socks"
 @onready var shoes = $"../Person/shoes"
+
+#outfit code
+var current_hair = -1 #not wearing
+var current_tops = -1
+var current_bottoms = -1
+var current_dresses = -1
+var current_socks = -1
+var current_shoes = -1
 
 func _ready():
 	hide_closet()
@@ -40,18 +49,36 @@ func show_clothes(index):#show clothes from the index
 		c.visible = (i == index)
 		
 func wear_hair(index):
+	if current_hair == index:#click again to unwear
+		current_hair = -1
+		for h in hair.get_children():
+			h.visible = false
+		return
+	current_hair = index
 	for i in range(hair.get_child_count()):
 		var h = hair.get_child(i)
 		h.visible = (i == index)
 
 func wear_top(index):
 	hide_group(dresses)
-	for i in range(tops.get_child_count()):
+	if current_tops == index:#click again to unwear
+		current_tops = -1
+		for t in tops.get_children():
+			t.visible = false
+		return
+	current_tops = index
+	for i in range(tops.get_child_count()):#click to wear
 		var t = tops.get_child(i)
 		t.visible = (i == index)
 		
 func wear_bottoms(index):
 	hide_group(dresses)
+	if current_bottoms == index:#click again to unwear
+		current_bottoms = -1
+		for b in bottoms.get_children():
+			b.visible = false
+		return
+	current_bottoms = index
 	for i in range(bottoms.get_child_count()):
 		var b = bottoms.get_child(i)
 		b.visible = (i == index)
@@ -59,16 +86,34 @@ func wear_bottoms(index):
 func wear_dresses(index):
 	hide_group(tops)
 	hide_group(bottoms)
+	if current_dresses == index:#click again to unwear
+		current_dresses = -1
+		for d in dresses.get_children():
+			d.visible = false
+		return
+	current_dresses = index
 	for i in range(dresses.get_child_count()):
 		var d = dresses.get_child(i)
 		d.visible = (i == index)
 		
 func wear_socks(index):
+	if current_socks == index:#click again to unwear
+		current_socks = -1
+		for so in socks.get_children():
+			so.visible = false
+		return
+	current_socks = index
 	for i in range(socks.get_child_count()):
 		var so = socks.get_child(i)
 		so.visible = (i == index)
 		
 func wear_shoes(index):
+	if current_shoes == index:#click again to unwear
+		current_shoes = -1
+		for sh in tops.get_children():
+			sh.visible = false
+		return
+	current_shoes = index
 	for i in range(shoes.get_child_count()):
 		var sh = shoes.get_child(i)
 		sh.visible = (i == index)
